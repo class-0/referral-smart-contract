f1eab6cb6683f9db21ce367630942af99587a2a2
Ace <164004703+class-0@users.noreply.github.com>
2024-10-12 16:58:06 -0400

    Update 2_deploy.js

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -11,4 +11,3 @@ module.exports = function(deployer) {
 
   deployer.deploy(Referral, decimals, referralBonus, secondsUntilInactive, onlyRewardActiveReferrers, levelRate, refereeBonusRateMap);
 };
-
